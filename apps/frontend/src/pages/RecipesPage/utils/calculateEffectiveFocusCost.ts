7f1f693398547d4e9b3082695e748cdee7939b58
Niko464 <58821733+Niko464@users.noreply.github.com>
2025-08-26 22:25:09 +0800

    save

--- apps/frontend/src/pages/RecipesPage/utils/calculateEffectiveFocusCost.ts
+++ apps/frontend/src/pages/RecipesPage/utils/calculateEffectiveFocusCost.ts
@@ -1,33 +1,53 @@
 import type { PlayerSpecializationStats, Recipe } from "@albion_online/common";
 
-/*
-Soup
-Salad
-Pie
-Roast
-Omelette
-Stew
-Sandwich
-Ingredient
-Butcher
-*/
+const getBonuses = (
+  craftingCategory: string
+): {
+  masteryBonus: number;
+  correctBranchBonus: number;
+  wrongBranchBonus: number;
+} => {
+  switch (craftingCategory) {
+    case "food":
+      return {
+        masteryBonus: 0.3,
+        correctBranchBonus: 2.5,
+        wrongBranchBonus: 0.3,
+      };
+    case "potion":
+      return {
+        masteryBonus: 0.3,
+        correctBranchBonus: 2.5,
+        wrongBranchBonus: 0.18,
+      };
+    default:
+      throw new Error("getBonus: Unknown crafting category");
+  }
+};
 // FORMULA FROM: https://forum.albiononline.com/index.php/Thread/126568-Cooking-Focus-Cost-Formula/
 // Focus required = Focus base cost * 0.5^((Mastery level * 0.3 + Specialization level A * Specialization bonus A/100 + Specialization level B * Specialization bonus B/100 + ... + Specialization level N * Specialization bonus N/100 ) / 100)
 // = 610 * 0,5^((2,8*U20 + 0,3*(U16+U17+U18+U19+U21+U22+U23+U24+U25))/100)
 export const getEffectiveFocusCost = (
   recipe: Recipe,
-  playerSpecStats: PlayerSpecializationStats | null,
+  playerSpecStats: PlayerSpecializationStats | null
 ): number => {
   if (!recipe.focus) return 0;
   if (!playerSpecStats) return recipe.focus * recipe.quantity;
+  const { masteryBonus, correctBranchBonus, wrongBranchBonus } = getBonuses(
+    recipe.craftingCategory
+  );
   const branchKeys = Object.keys(playerSpecStats.specializations);
 
   const specializationTotal = branchKeys.reduce((total, key) => {
     const isRecipeSameBranch = recipe.specializationBranchName === key;
-    const specializationBonus = isRecipeSameBranch ? 2.8 : 0.3;
+    const specializationBonus = isRecipeSameBranch
+      ? correctBranchBonus + wrongBranchBonus
+      : wrongBranchBonus;
     const specializationLevel = playerSpecStats.specializations[key] || 0;
-    return total + (specializationLevel * specializationBonus);
-  }, playerSpecStats.mastery * 0.3);
+    return total + specializationLevel * specializationBonus;
+  }, playerSpecStats.mastery * masteryBonus);
 
-  return recipe.focus * recipe.quantity * Math.pow(0.5, specializationTotal / 100);
+  return (
+    recipe.focus * recipe.quantity * Math.pow(0.5, specializationTotal / 100)
+  );
 };
--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,3 +1,2 @@
 export * from "./types";
-export * from "./cooking_recipes";
 export * from "./config";
